1007bb77d6e7a43ffef45c41d536d605b32b6dd2
Thomas Ogolla <tomogolla637@gmail.com>
2025-07-22 21:17:16 +0300

    added models for user, role, conversation, and message

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -115,7 +115,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISION_CLASSES': [
-        'rest_framework.permision.IsAuthenticated',
+        'rest_framework.permisions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
