981fd6aa9136f228b6ea601c60c38190556e516e
Thomas Ogolla <tomogolla637@gmail.com>
2025-07-26 16:15:54 +0300

    checks pagination and filtering, testing api endpoitns with postman, added  json collections

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -127,8 +127,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',
-    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PERMISION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
